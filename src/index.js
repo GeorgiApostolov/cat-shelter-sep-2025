5dde8fd2909c4ade8a8de034338f36fe10e43f64
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2025-09-24 20:13:34 +0300

    Add create breed page

--- src/index.js
+++ src/index.js
@@ -1,29 +1,58 @@
 import http from "http";
 import fs from "fs/promises";
 
-const server = http.createServer(async (req, res) => {
-  const homeHtml = await fs.readFile("./src/views/home/index.html", {
+async function homeView() {
+  const html = await fs.readFile("./src/views/home/index.html", {
     encoding: "utf-8",
   });
 
-  if (req.url === `/`) {
-    res.writeHead(200, {
-      "content-type": "text/html",
-    });
+  return html;
+}
 
-    res.write(homeHtml);
-  } else if (req.url === `/styles/site.css`) {
-    const siteCss = await fs.readFile("./src/styles/site.css", {
-      encoding: "utf-8",
-    });
+async function addBreedView() {
+  const html = await fs.readFile("./src/views/addBreed.html", {
+    encoding: "utf-8",
+  });
+  return html;
+}
 
-    res.writeHead(200, {
-      "content-type": "text/css",
-    });
+const server = http.createServer(async (req, res) => {
+  switch (req.url) {
+    case "/":
+      const html = await homeView();
 
-    res.write(siteCss);
+      res.writeHead(200, {
+        "content-type": "text/html",
+      });
+
+      res.write(html);
+      break;
+
+    case "/styles/site.css":
+      const siteCss = await fs.readFile("./src/styles/site.css", {
+        encoding: "utf-8",
+      });
+
+      res.writeHead(200, {
+        "content-type": "text/css",
+      });
+
+      res.write(siteCss);
+      break;
+
+    case "/cats/add-breed":
+      const addBreed = await addBreedView();
+
+      res.writeHead(200, {
+        "content-type": "text/html",
+      });
+
+      res.write(addBreed);
+      break;
+    default:
+      res.end();
+      break;
   }
-
   res.end();
 });
 
